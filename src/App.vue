11c5b915c2acb6fbcc479c70e8051880e38ed3fa
Lihu Kim <lh.ines.kim@gmail.com>
2021-09-29 23:30:18 +0900

    CSS : Basic css done

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,45 @@
 <template>
-  <header>
-    <h3>오늘 날짜</h3>
-    <p>날씨</p>
-  </header>
+  <div class="app-wrap">
+    <header class="header">
+      <button class="prev">
+        <i class="fas fa-angle-left"></i>
+      </button>
 
-  <section>
-    <input type="text" placeholder="오늘 해야할 일이 뭐더라?" />
+      <div class="date">
+        <span>MONDAY, </span>
+        March 14th
+      </div>
 
-    <div>
-      할 일 목록
-    </div>
-  </section>
+      <button class="next">
+        <i class="fas fa-angle-right"></i>
+      </button>
+    </header>
 
-  <footer>
-    <!-- 남은 할일 percent에 따라 응원하는 글귀가 뜨도록 한다 -->
-    <p>text</p>
+    <section class="list">
+      <div class="writeInput">
+        <input type="text" placeholder="오늘 해야할 일이 뭐더라?" />
+        <button>
+          <i class="fas fa-plus"></i>
+        </button>
+      </div>
 
-    <div>percentage</div>
-  </footer>
+      <div>
+        할 일 목록
+      </div>
+    </section>
+
+    <footer class="footer">
+      <!-- 남은 할일 percent에 따라 응원하는 글귀가 뜨도록 한다 -->
+      <div class="list-left">
+        3개 남았어요,
+        <span class="cheer-text">조금만 더 힘내요! </span>
+      </div>
+
+      <div class="percentage">
+        <span></span>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -27,4 +49,6 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+@import './style.scss';
+</style>
